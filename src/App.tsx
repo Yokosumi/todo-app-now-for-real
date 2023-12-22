4b9835573cd1e3681b2b4c6715cdead5d954950c
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-12-23 00:53:33 +0100

    Add DaisyUI plugin to tailwind.config.js and update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 function App() {
-  return (
-    <>
-      <div className="flex flex-col gap-10 justify-center items-center text-white my-4">
-        <h1 className="text-5xl">Template: React + TypeScript + Tailwind</h1>
-        <p className="w-[70ch] text-3xl">
-          This template is powered by Vite, a fast and opinionated frontend
-          build tool. Leveraging the efficiency of React for dynamic UIs, the
-          robustness of TypeScript for static typing, and the utility-first
-          approach of Tailwind CSS for styling, this combination crafted a
-          modern and streamlined development environment.
-        </p>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center min-h-screen ">
+                <div className="p-4 bg-white rounded shadow-md w-64">
+                    <h1 className="text-2xl font-bold mb-4">Todo List</h1>
+                    <input
+                        className="border p-2 w-full"
+                        type="text"
+                        placeholder="Add todo"
+                    />
+                    <div className="mt-4">
+                        <h2 className="font-bold mb-2">Todos:</h2>
+                        <ul>
+                            <li className="border p-2 mb-2">Todo 1</li>
+                            <li className="border p-2 mb-2">Todo 2</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default App;
+export default App
