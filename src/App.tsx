ffc9e337715efaf3e4f66a4f0ede574d36afae06
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-12-23 01:48:18 +0100

    Add react-icons library and implement todo list functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,62 @@
+import { useState } from 'react'
+import { MdDelete } from 'react-icons/md'
+
+type TodoListType = string[]
+type NewTodoType = string
+
 function App() {
+    const [todos, setTodos] = useState([] as TodoListType)
+    const [newTodo, setNewTodo] = useState('' as NewTodoType)
+
+    const handleAddTodo = () => {
+        if (newTodo !== '') {
+            setTodos([...todos, newTodo])
+            setNewTodo('')
+        }
+    }
+
+    const handleDeleteTodo = (index: number | any) => {
+        const newTodos = [...todos]
+        newTodos.splice(index, 1)
+        setTodos(newTodos)
+    }
+
     return (
         <>
             <div className="flex flex-col items-center justify-center min-h-screen ">
                 <div className="p-4 bg-white rounded shadow-md w-64">
                     <h1 className="text-2xl font-bold mb-4">Todo List</h1>
                     <input
+                        onChange={(e) => setNewTodo(e.target.value)}
                         className="border p-2 w-full"
                         type="text"
                         placeholder="Add todo"
                     />
+                    <button
+                        onClick={handleAddTodo}
+                        className="button button-primary"
+                    >
+                        Add Todo
+                    </button>
                     <div className="mt-4">
                         <h2 className="font-bold mb-2">Todos:</h2>
                         <ul>
-                            <li className="border p-2 mb-2">Todo 1</li>
-                            <li className="border p-2 mb-2">Todo 2</li>
+                            {todos.map((todo, index) => (
+                                <>
+                                    <div
+                                        key={index}
+                                        className="flex justify-around gap-3 border p-2 mb-2"
+                                    >
+                                        <p>{todo}</p>
+                                        <button
+                                            onClick={handleDeleteTodo}
+                                            className="button button-primary"
+                                        >
+                                            <MdDelete />
+                                        </button>
+                                    </div>
+                                </>
+                            ))}
                         </ul>
                     </div>
                 </div>
