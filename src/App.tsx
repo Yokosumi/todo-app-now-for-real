79c6e9264e1a8270b668314360bab144add60a14
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-12-23 02:46:27 +0100

    Add icons and editing functionality to todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import { useState } from 'react'
 import { MdDelete } from 'react-icons/md'
+import { IoIosAdd } from 'react-icons/io'
+import { MdEdit } from 'react-icons/md'
+import { FaSave } from 'react-icons/fa'
 
 type TodoListType = string[]
 type NewTodoType = string
+type EditingType = boolean
+type EditTodoType = string
 
 function App() {
     const [todos, setTodos] = useState([] as TodoListType)
     const [newTodo, setNewTodo] = useState('' as NewTodoType)
+    const [editing, setEditing] = useState(false as EditingType)
+    const [editTodo, setEditTodo] = useState('' as EditTodoType)
+
+    const handleEditTodo = (todo: string) => {
+        setEditing(true)
+        setEditTodo(todo)
+    }
+
+    const handleUpdateTodo = () => {
+        setEditing(false)
+        setEditTodo('')
+    }
 
     const handleAddTodo = () => {
         if (newTodo !== '') {
@@ -26,18 +43,20 @@ function App() {
             <div className="flex flex-col items-center justify-center min-h-screen ">
                 <div className="p-4 bg-white rounded shadow-md w-64">
                     <h1 className="text-2xl font-bold mb-4">Todo List</h1>
-                    <input
-                        onChange={(e) => setNewTodo(e.target.value)}
-                        className="border p-2 w-full"
-                        type="text"
-                        placeholder="Add todo"
-                    />
-                    <button
-                        onClick={handleAddTodo}
-                        className="button button-primary"
-                    >
-                        Add Todo
-                    </button>
+                    <div className="flex gap-3">
+                        <input
+                            onChange={(e) => setNewTodo(e.target.value)}
+                            className="border p-2 w-full"
+                            type="text"
+                            placeholder="Add todo"
+                        />
+                        <button
+                            onClick={handleAddTodo}
+                            className="button button-primary"
+                        >
+                            <IoIosAdd className="text-3xl text-black" />
+                        </button>
+                    </div>
                     <div className="mt-4">
                         <h2 className="font-bold mb-2">Todos:</h2>
                         <ul>
@@ -45,14 +64,38 @@ function App() {
                                 <>
                                     <div
                                         key={index}
-                                        className="flex justify-around gap-3 border p-2 mb-2"
+                                        className="flex justify-around gap-1 border p-2 mb-2"
                                     >
-                                        <p>{todo}</p>
+                                        {editing && todo === editTodo ? (
+                                            <textarea
+                                                value={editTodo}
+                                                onChange={(e) =>
+                                                    setEditTodo(e.target.value)
+                                                }
+                                            />
+                                        ) : (
+                                            <p>{todo}</p>
+                                        )}
+
                                         <button
                                             onClick={handleDeleteTodo}
                                             className="button button-primary"
                                         >
                                             <MdDelete />
+                                        </button>
+                                        <button
+                                            onClick={() => handleEditTodo(todo)}
+                                        >
+                                            <MdEdit />
+                                        </button>
+                                        <button>
+                                            {editing && todo === editTodo && (
+                                                <button
+                                                    onClick={handleUpdateTodo}
+                                                >
+                                                    <FaSave />
+                                                </button>
+                                            )}
                                         </button>
                                     </div>
                                 </>
