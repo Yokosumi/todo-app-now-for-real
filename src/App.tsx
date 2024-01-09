783ce6a561e35fa1b5607f4e86df8349e7cbb146
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-01-09 15:49:16 +0100

    reset editing function

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,19 @@
 import { useState } from 'react'
 import { MdDelete } from 'react-icons/md'
 import { IoIosAdd } from 'react-icons/io'
-import { MdEdit } from 'react-icons/md'
-import { FaSave } from 'react-icons/fa'
+// import { MdEdit } from 'react-icons/md'
+// import { FaSave } from 'react-icons/fa'
+
+//TODO: make a working edit function
 
 type TodoListType = string[]
 type NewTodoType = string
-type EditingType = boolean
-type EditTodoType = string
+// type EditingType = boolean
+// type EditTodoType = string
 
 function App() {
     const [todos, setTodos] = useState([] as TodoListType)
     const [newTodo, setNewTodo] = useState('' as NewTodoType)
-    const [editing, setEditing] = useState(false as EditingType)
-    const [editTodo, setEditTodo] = useState('' as EditTodoType)
-
-    const handleEditTodo = (todo: string) => {
-        setEditing(true)
-        setEditTodo(todo)
-    }
-
-    const handleUpdateTodo = () => {
-        setEditing(false)
-        setEditTodo('')
-    }
 
     const handleAddTodo = () => {
         if (newTodo !== '') {
@@ -66,36 +56,13 @@ function App() {
                                         key={index}
                                         className="flex justify-around gap-1 border p-2 mb-2"
                                     >
-                                        {editing && todo === editTodo ? (
-                                            <textarea
-                                                value={editTodo}
-                                                onChange={(e) =>
-                                                    setEditTodo(e.target.value)
-                                                }
-                                            />
-                                        ) : (
-                                            <p>{todo}</p>
-                                        )}
+                                        <p>{todo}</p>
 
                                         <button
                                             onClick={handleDeleteTodo}
                                             className="button button-primary"
                                         >
                                             <MdDelete />
-                                        </button>
-                                        <button
-                                            onClick={() => handleEditTodo(todo)}
-                                        >
-                                            <MdEdit />
-                                        </button>
-                                        <button>
-                                            {editing && todo === editTodo && (
-                                                <button
-                                                    onClick={handleUpdateTodo}
-                                                >
-                                                    <FaSave />
-                                                </button>
-                                            )}
                                         </button>
                                     </div>
                                 </>
