4a6d5eb46df7d7059402bc194946ea0b0fd34c2c
Selman Ciftci <selman.ciftci.coding@gmail.com>
2024-01-23 10:06:56 +0100

    added habibi font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ export default {
         extend: {
             fontFamily: {
                 inter: ['Inter', 'sans-serif'],
+                habibi: ['Habibi', 'serif'],
             },
         },
     },
